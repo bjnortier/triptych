58c60b1c98ddd74153c7589827fd5a363819f320
Ben Nortier <ben@bjnortier.com>
2015-04-14 15:21:44 +0200

    DOM Scene + View

--- test/functional/src/events.test.js
+++ test/functional/src/events.test.js
@@ -1,18 +1,23 @@
 var flow = require('../../..');
 var $ = flow.$;
 var EmptyModel = flow.Model;
-var Scene = flow.scenes.ThreeJSScene;
+var ThreeJSScene = flow.scenes.ThreeJSScene;
+var DOMScene = flow.scenes.DOMScene;
 var AxesView = flow.views.AxesView;
 
 var CubeView = require('./CubeView');
+var EventDebugView = require('./EventDebugView');
 
-var sceneOptions = {
+var threeJSSceneOptions = {
   cameraPosition: {
     x: 1, y: 2, z: 1
   }
 };
-var scene = new Scene($('#viewport'), sceneOptions);
+var threeJSScene = new ThreeJSScene($('#viewport'), threeJSSceneOptions);
+var domScene = new DOMScene($('#dom'));
 
 var model = new EmptyModel();
-new AxesView(model, scene);
-new CubeView(model, scene);
+new AxesView(model, threeJSScene);
+new CubeView(model, threeJSScene);
+
+new EventDebugView(model, domScene);
